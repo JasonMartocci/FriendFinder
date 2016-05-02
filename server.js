c57e04838d6ae59373dffa0e5494d6871d45e7b4
Jason Martocci <martoccijason@gmail.com>
2016-05-02 15:51:49 -0400

    updated the scripts

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
+
+//set up routes
+require('./app/routing/api-routes.js')(app);
+require('./app/routing/html-routes.js')(app);
+
+//start server
+app.listen(PORT, function(){
+	console.log('Listening at port: '+ PORT);
+});
