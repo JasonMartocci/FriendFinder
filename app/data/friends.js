97a5b2caca571948cab1373557ea5ee0df2af278
Jason Martocci <martoccijason@gmail.com>
2016-05-03 11:07:09 -0400

    making progress

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,15 +1,21 @@
+var friends = [
+
 {
-  "name":"Ahmed",
-  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-  "scores":[
-     5,
-     1,
-     4,
-     4,
-     5,
-     1,
-     2,
-     5,
-     4,
-     1
+  name: 'Billy',
+  photo: 'http://www.peta.org/wp-content/uploads/2016/02/Goats-maximili-602x403.jpg',
+  scores: [2, 3, 4, 5, 3, 2, 1, 2, 3, 5]
+},
+{
+  name: 'Donald',
+  photo: 'http://c1.nrostatic.com/sites/default/files/uploaded/donald-trump-incapable-of-embarrassment-r_0.jpg',
+  scores: [1, 1, 2, 3, 1, 4, 5, 2, 3, 1]
+},
+{
+  name: 'Bernie',
+  photo: 'https://01.img.society6.com/society6/img/nQs0Oq4hVzuNFynfZJQMX-7v9Bs/w_700/artwork/~artwork/s6-0066/a/27326061_9971749/~~/bernie-and-kittens-prints.jpg',
+  scores: [4, 5, 4, 2, 3, 4, 3, 5, 3, 4]
 }
+
+];
+
+module.exports = friends;
